4080faef08df14b2f42466eeb151416b540b66e6
13783455732 <13783455732@163.com>
2021-05-24 10:02:08 +0800

    cs

--- lib/statefull_group_page.dart
+++ lib/statefull_group_page.dart
@@ -18,6 +18,7 @@ class StateFulGroup extends StatefulWidget {
 
 class _StateFulGroupState extends State<StateFulGroup> {
   int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = TextStyle(fontSize: 20);
@@ -40,64 +41,118 @@ class _StateFulGroupState extends State<StateFulGroup> {
         appBar: AppBar(title: Text('StateLessWidget与基础组件')),
         bottomNavigationBar: BottomNavigationBar(
             currentIndex: _currentIndex,
-            onTap: (index){
+            onTap: (index) {
               setState(() {
-                _currentIndex=index;
+                _currentIndex = index;
               });
             },
-            items:[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home,color: Colors.grey,),
-            activeIcon: Icon(Icons.home,color:Colors.blue),
-              label: '首页'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.list,color: Colors.grey,),
-              activeIcon: Icon(Icons.list,color:Colors.blue),
-              label: '列表'
-          )
-        ]
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.home, color: Colors.blue),
+                  label: '首页'),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.list,
+                    color: Colors.grey,
+                  ),
+                  activeIcon: Icon(Icons.list, color: Colors.blue),
+                  label: '列表')
+            ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Text('点我'),
         ),
-        body: _currentIndex==0?Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(color: Colors.white),
-          child: Column(
-            children: <Widget>[
-              Text('I am Text', style: textStyle),
-              Icon(Icons.android,size: 50,color: Colors.red,),
-              CloseButton(),
-              BackButton(),
-              Chip(
-                  avatar: Icon(Icons.people),
-                  label: Text('123123123')
-              ),
-              Divider(
-                height: 10,
-                indent: 10,
-                color: Colors.orange,
-              ),
-              Card(
-                color: Colors.blue,
-                elevation: 5,
-                margin: EdgeInsets.all(10),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Text('321321',style: textStyle,),
+        body: _currentIndex == 0
+            ? RefreshIndicator(
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Column(
+                        children: <Widget>[
+                          Image.network(
+                            'https://image.awehome.com/data/uploads/hot-school/MTU2MDczNzEzMy05MjI=.jpg',
+                            width: 100,
+                            height: 100,
+                          ),
+                          TextField(
+                            //输入样式
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              hintText: '请输入',
+                              hintStyle: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          Container(
+                            height: 200,
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(color: Colors.lightBlueAccent),
+                            child: PageView(
+                              children: [
+                                _image('https://image.awehome.com/data/uploads/hot-school/MTU2MDczNzEzMy05MjI=.jpg'),
+                                _item('Page1',Colors.deepPurple),
+                                _item('Page2',Colors.greenAccent),
+                                _item('Page3',Colors.red),
+                                _item('Page4',Colors.deepOrangeAccent)
+                              ],
+                            ),
+                          ),
+                          /*Text('I am Text', style: textStyle),
+                  Icon(Icons.android,size: 50,color: Colors.red,),
+                  CloseButton(),
+                  BackButton(),
+                  Chip(
+                      avatar: Icon(Icons.people),
+                      label: Text('123123123')
+                  ),
+                  Divider(
+                    height: 10,
+                    indent: 10,
+                    color: Colors.orange,
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    elevation: 5,
+                    margin: EdgeInsets.all(10),
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      child: Text('321321',style: textStyle,),
+                    ),
+                  ),
+                  AlertDialog(
+                    title: Text('那我走?'),
+                    content: Text('李某: 唉~'),
+                  ),*/
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              AlertDialog(
-                title: Text('那我走?'),
-                content: Text('李某: 唉~'),
-              ),
-            ],
-          ),
-        ):Text('列表页'),
+                onRefresh: _handleRefresh)
+            : Text('列表页'),
       ),
     );
   }
+
+  Future<Null> _handleRefresh() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    return null;
   }
 
+  _item(String title, Color color) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(color: color),
+      child: Text(title,style:TextStyle(fontSize: 22,color:Colors.white)),
+    );
+  }
 
-
-
-
+  _image(String src) {
+    return Image.network(src,fit: BoxFit.cover,);
+  }
+}
