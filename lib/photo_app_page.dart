4e85cafd4920a7fe318b9afe0cdff0bc50ec8e39
master <13783455732@163.com>
2021-05-30 13:18:56 +0800

    cs

--- lib/photo_app_page.dart
+++ lib/photo_app_page.dart
@@ -12,15 +12,16 @@ class PhotoApp extends StatefulWidget {
 }
 
 class _PhotoAppState extends State<PhotoApp> {
-  File _image;
+  List _images = [];
   final picker = ImagePicker();
 
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
+  Future getImage(bool isTackPhoto) async {
+    Navigator.pop(context);
+    final pickedFile = await picker.getImage(source: isTackPhoto?ImageSource.camera:ImageSource.gallery);
 
     setState(() {
       if (pickedFile != null) {
-        _image = File(pickedFile.path);
+        _images.add(File(pickedFile.path));
       } else {
         print('No image selected.');
       }
@@ -31,7 +32,7 @@ class _PhotoAppState extends State<PhotoApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('app拍照开发'),
+        title: Text('app拍照开发123'),
         leading: GestureDetector(
           onTap: () {
             Navigator.pop(context);
@@ -40,13 +41,70 @@ class _PhotoAppState extends State<PhotoApp> {
         ),
       ),
       body: Center(
-        child: _image == null ? Text('No image selected.') : Image.file(_image),
+        child: Wrap(
+          spacing: 5,
+          runSpacing: 5,
+          children: _genImages(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
+        onPressed: _pickImage,
+        tooltip: '选择图片',
         child: Icon(Icons.add_a_photo),
       ),
     );
+  }
+
+  void _pickImage() {
+    showModalBottomSheet(context: context, builder: (context)=>Container(
+      height: 160,
+      child: Column(
+        children: [
+          _item('拍照',true),
+          _item('从相册选择', false),
+        ],
+      ),
+    ));
+  }
+
+  _item(String title, bool isTakePhoto) {
+    return GestureDetector(
+      child: ListTile(
+        leading: Icon(isTakePhoto?Icons.camera_alt:Icons.photo_library),
+        title:Text(title),
+        onTap: () => getImage(isTakePhoto),
+      ),
+    );
+  }
+
+  _genImages() {
+    return _images.map((file) {
+      return Stack(
+        children: [
+          ClipRRect(
+            // 圆角
+            borderRadius: BorderRadius.circular(5),
+            child: Image.file(file,width: 120,height:90,fit:BoxFit.fill),
+          ),
+          Positioned(
+              right: 5,
+              top:5,
+              child: GestureDetector(
+                onTap:(){
+                  setState(() {
+                    _images.remove(file);
+                  });
+                },
+                child: ClipOval(
+                  child: Container(
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(color:Colors.black54),
+                    child: Icon(Icons.close, size: 18, color: Colors.white)
+                  ),
+                ),
+              ))
+        ],
+      );
+    }).toList();
   }
 }
